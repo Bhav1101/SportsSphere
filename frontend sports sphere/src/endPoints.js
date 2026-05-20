807811c83f41bc461988eae92bf6229aba959b96
Bhav1101 <bhavjot360@gmail.com>
2026-05-20 10:16:22 +0530

    Update endPoints.js

--- frontend sports sphere/src/endPoints.js
+++ frontend sports sphere/src/endPoints.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000"
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://sportssphere-1.onrender.com"
 
 export const BASE_URL = `${API_BASE_URL.replace(/\/$/, "")}/`
 
